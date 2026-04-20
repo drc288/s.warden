8d5b88b7dceb083e3d0fe44b8dcbafb0f22574c2
drc288 <davidroserocalle@gmail.com>
2026-04-19 23:38:44 -0500

    feat: re.estrcucture dependency file, add groq engine

--- src/modules/incidents/presentation/dependencies.py
+++ src/modules/incidents/presentation/dependencies.py
@@ -1,14 +1,26 @@
 from functools import lru_cache
 
-from src.modules.incidents.application.interfaces import IIncidentRepository
-from src.modules.incidents.application.use_cases import IncidentUseCases
+from src.modules.incidents.application.interfaces import (
+    IIncidentRepository,
+    IReasoningEngine,
+)
+from src.modules.incidents.application.use_cases import ProcessIncidentUseCase
+from src.modules.incidents.infrastructure.reasoning import GroqReasoningEngine
 from src.modules.incidents.infrastructure.repositories import InMemoryIncidentRepository
 
 
-_repository = InMemoryIncidentRepository()
-
+@lru_cache(maxsize=1)
 def get_incident_repository() -> IIncidentRepository:
-    return _repository
+    return InMemoryIncidentRepository()
 
-def get_incident_use_cases() -> IncidentUseCases:
-    return IncidentUseCases(get_incident_repository())
+
+@lru_cache(maxsize=1)
+def get_reasoning_engine() -> IReasoningEngine:
+    return GroqReasoningEngine()
+
+
+def get_process_incident_use_case() -> ProcessIncidentUseCase:
+    return ProcessIncidentUseCase(
+        reasoning=get_reasoning_engine(),
+        repository=get_incident_repository(),
+    )
